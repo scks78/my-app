2b707d3d0bcc01dff0cc0ce310ddcde39f435aaa
scks78 <stanley@primalux.com.sg>
2018-04-20 15:45:27 +0800

    Add RouteModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { RouterModule,Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { JokeComponent } from './joke/joke.component';
 import { JokelistComponent } from './jokelist/jokelist.component';
 import { JokeFormComponent } from './joke-form/joke-form.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const appRoutes:Routes =[
+  {path: '',component:HomeComponent},
+  {path: 'jokes',component:JokelistComponent},
+  {path: 'joke-list',redirectTo:'jokes'},
+  {path: '**',component:PageNotFoundComponent},
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     JokeComponent,
     JokelistComponent,
-    JokeFormComponent
+    JokeFormComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes,),
     BrowserModule
   ],
   providers: [],
